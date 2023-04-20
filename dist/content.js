e30602b8a62a07abe7882fd084e0f73835569d5f
CoreyAI <coreyiaquinta@gmail.com>
2023-04-20 03:47:43 -0400

    Expanded module with placeholder templates.

--- dist/content.js
+++ dist/content.js
@@ -1,18 +1,47 @@
-const contentContainer = document.getElementById("content-container");
+// const contentContainer = document.getElementById("content-container");
 
-const resetContainer = contentContainer.innerHTML = "";
+// const resetContainer = contentContainer.innerHTML = "";
+
+const contentContainer = () => {
+  const contentContainer = document.createElement("div");
+  contentContainer.setAttribute("id", "content-container");
+  return contentContainer;
+}
 
 const weatherTemplate = () => {
-  const weatherContainer = document.createElement("div");
-  weatherContainer.setAttribute("class", "weather-container");
 
-  // Test/TODO
-  weatherContainer.innerHTML = 'Weather App Bubble';
+
+  const weatherContainer = document.createElement("div");
+  weatherContainer.setAttribute("id", "weather-container");
+  // weatherContainer.innerHTML = 'Weather App Bubble';
+
+  const bubbleTopRow = document.createElement("div");
+  bubbleTopRow.setAttribute("class", "bubble-item");
+  bubbleTopRow.setAttribute("id", "bubble-top-row");
+  const bubbleLeftColumn = document.createElement("div");
+  bubbleLeftColumn.setAttribute("class", "bubble-item");
+  bubbleLeftColumn.setAttribute("id", "bubble-left-column");
+  const bubbleRightColumn = document.createElement("div");
+  bubbleRightColumn.setAttribute("class", "bubble-item");
+  bubbleRightColumn.setAttribute("id", "bubble-right-column");
+
+  weatherContainer.appendChild(bubbleTopRow);
+  weatherContainer.appendChild(bubbleLeftColumn);
+  weatherContainer.appendChild(bubbleRightColumn);
+
+
   return weatherContainer;
 }
 
-const addElementToContentContainer= (e) => {
+const bubbleTopRow = () => {
+  const container = document.getElementById("bubble-top-row");
+
+}
+
+const addElementToContentContainer = (e) => {
   contentContainer.appendChild(e);
 }
 
-addElementToContentContainer(weatherTemplate());
+// addElementToContentContainer(weatherTemplate());
+
+export { weatherTemplate, contentContainer };
