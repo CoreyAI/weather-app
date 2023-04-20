44e3bbc27b9ce649a7c5b6bb04ec5388732f4055
CoreyAI <coreyiaquinta@gmail.com>
2023-04-20 13:17:28 -0400

    Added location bar event

--- src/index.js
+++ src/index.js
@@ -7,3 +7,23 @@ const weatherContainer = Content.weatherContainer();
 
 pageLoad.appendElement(contentContainer);
 contentContainer.appendChild(weatherContainer);
+
+const locationBar = document.getElementById("bubble-top-row");
+const locationElement = document.getElementById("location");
+let locationName = '';
+locationElement.addEventListener("click", function() {
+  console.log("location has been clicked!");
+  Content.setSearchClickFlag();
+  console.log(Content.getSearchClickFlag())
+
+  locationName = locationElement.innerText;
+
+  //WARNING: Can't remove if user cancels the search! Using this method for now
+  //         to proceed with developing the logic. 
+  locationBar.removeChild(locationBar.firstChild);
+  Content.displayLocationOrSearch(locationBar);
+});
+
+//TODO: Possibly create async/await for search field generated from clicking
+//      on location name in order to allow for another event listener to 
+//      register form buttons.
